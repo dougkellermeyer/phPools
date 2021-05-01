dedaade065e19c205cc35273146efaa3efc4de6f
Doug Kellermeyer <doug.kellermeyer@gmail.com>
2021-05-01 19:42:11 -0400

    changed height

--- src/components/main.js
+++ src/components/main.js
@@ -370,7 +370,7 @@ const withStyles = makeStyles(() => ({
         "@media(max-width:600px)":{
             flexDirection: "column",
             marginBottom: "20px",
-            maxHeight: "93px"
+            maxHeight: "110px"
         }
     },
     bbbLink: {
